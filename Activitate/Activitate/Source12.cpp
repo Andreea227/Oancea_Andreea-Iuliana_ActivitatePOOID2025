3a1cc20081d225e30ef0f5ced6c4f220abd0f47f
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2025-12-09 17:58:58 +0200

    Adaugare constructor cu parametri

--- Activitate/Activitate/Source12.cpp
+++ Activitate/Activitate/Source12.cpp
@@ -17,5 +17,14 @@ public:
         this->ani = 0;
         this->consumKm = nullptr;
     }
+    Auto(string marca, string model, int ani, float* consumKm) {
+        this->marca = marca;
+        this->model = model;
+        this->ani = ani;
 
+        this->consumKm = new float[ani];
+        for (int i = 0; i < ani; i++) {
+            this->consumKm[i] = consumKm[i];
+        }
+    }
 };
