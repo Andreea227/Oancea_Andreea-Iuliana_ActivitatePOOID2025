c4e52964a8e9882c7be69272b701e572812188cf
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2025-12-11 20:47:16 +0200

    Adaugare constructor cu parametri

--- Activitate/Activitate/Source14.cpp
+++ Activitate/Activitate/Source14.cpp
@@ -17,4 +17,14 @@ public:
         this->nrNote = 0;
         this->frecvente = nullptr;
     }
+    InstrumentMuzical(string nume, float pret, int nrNote, int* frecvente) {
+        this->nume = nume;
+        this->pret = pret;
+        this->nrNote = nrNote;
+
+        this->frecvente = new int[nrNote];
+        for (int i = 0; i < nrNote; i++) {
+            this->frecvente[i] = frecvente[i];
+        }
+    }
 };
