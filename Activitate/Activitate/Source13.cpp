a0e9746131ef4a33927f8931295f0401c348c554
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2025-12-11 20:19:11 +0200

    Adaugare constructor cu parametri

--- Activitate/Activitate/Source13.cpp
+++ Activitate/Activitate/Source13.cpp
@@ -17,4 +17,14 @@ public:
         this->nrMateriale = 0;
         this->materiale = nullptr;
     }
+    Ornament(string nume, float pret, int nrMateriale, string* materiale) {
+        this->nume = nume;
+        this->pret = pret;
+        this->nrMateriale = nrMateriale;
+
+        this->materiale = new string[nrMateriale];
+        for (int i = 0; i < nrMateriale; i++) {
+            this->materiale[i] = materiale[i];
+        }
+    }
 };
