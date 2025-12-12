2aee8546b883c99383e4e792bc53b8409b410ae5
Oancea Andreea Iuliana <oanceaandreea21@stud.ase.ro>
2025-12-12 17:19:20 +0200

    Adaugare constructor cu parametri

--- Activitate/Activitate/Source16.cpp
+++ Activitate/Activitate/Source16.cpp
@@ -15,4 +15,14 @@ public:
         this->nrProduse = 0;
         this->preturi = nullptr;
     }
+
+    Magazin(string nume, int nrProduse, float* preturi) {
+        this->nume = nume;
+        this->nrProduse = nrProduse;
+
+        this->preturi = new float[nrProduse];
+        for (int i = 0; i < nrProduse; i++) {
+            this->preturi[i] = preturi[i];
+        }
+    }
 };
